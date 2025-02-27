c4218ac1affa1bc0c04c81de40c409187d047351
Adam Ngazzou <117188411+AdamNgazzou@users.noreply.github.com>
2025-02-27 18:52:56 +0100

    Update ex13.py

--- lab2/ex13.py
+++ lab2/ex13.py
@@ -1,16 +1,17 @@
 #ex13
 import random
+
 def play():
-    number = randint(1,100)
-    print(number)
-    while (True):
-        x=int(input("guess the number "))
-        if(x == number ):
-            print('Bravooo')
+    number = random.randint(1, 100) 
+    print(number)  
+    while True:
+        x = int(input("Guess the number: "))
+        if x == number:
+            print("Bravooo")
             break
-        elif(x>number):
-            print("too high")
+        elif x > number:
+            print("Too high")
         else:
-            print("too low")
+            print("Too low")
 
 play()
